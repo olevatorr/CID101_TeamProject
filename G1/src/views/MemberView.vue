680a97a87dfd2307090c3e6b532d15d00719daba
Rahaydn <lexea53@gmail.com>
2024-05-27 00:54:35 +0800

    修改

--- G1/src/views/MemberView.vue
+++ G1/src/views/MemberView.vue
@@ -1,25 +1,27 @@
 <template>
-    <main>
+    <main class="body-member">
         <section class="section section-member">
-            <div class="container">
-                <div class="member-box">
-                    <div class="member-box-img">
-                        <img src="/img/member/member2.jpg" alt="會員背景圖片" />
+            <div class="containerori">
+                <div class="member-box col-md-12">
+                    <div class="member-box-img col-sm-6 col-md-6">
+                        <img src="../../public/img/member/member2.jpg" alt="會員背景圖片">
                         <div class="member-logo-box">
-                            <img src="/img/member/member3.png" alt="logo" />
-                            <span>潔淨海洋<span></span>綠色明天</span>
-                            <button>加入會員</button>
+                            <img src="../../public/img/member/member3.png" alt="logo">
+                            <span>潔淨海洋綠色明天</span>
                         </div>
                     </div>
-                    <div class="member-txt">
+                    <div class="member-txt col-sm-6 col-md-6">
                         <div class="member-login">
                             <label>帳號</label>
-                            <input type="text" />
+                            <input type="text">
                             <label>密碼</label>
-                            <input type="password" name="" id="" />
+                            <input type="password" name="" id="">
                         </div>
-                        <button>登入</button>
-                        <a href="#">忘記密碼?</a>
+                        <div class="member-button">
+                            <button>登入</button>
+                            <button>加入會員</button>
+                        </div>
+                        <a href="#" class="forget">忘記密碼?</a>
                         <p>其他登入方式</p>
                         <a href="#"><i class="fa-brands fa-google-plus"></i></a>
                         <a href="#"><i class="fa-brands fa-facebook"></i></a>

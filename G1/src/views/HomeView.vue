d62f205eb0d95ca6d9594e8be4b75efda27e1b6c
chuhua1997 <chuhua1005@gmail.com>
2024-05-27 16:05:42 +0800

    help2

--- G1/src/views/HomeView.vue
+++ G1/src/views/HomeView.vue
@@ -42,142 +42,15 @@
           海洋垃圾一覽
         </h3>
         <div class="row">
-          <div class="col-12 col-lg-6">
-            <ul class="debris-sort" v-for="sort in hebrisSort" :key="sort.id">
-              <li>
-                <span class="material-symbols-outlined">line_end</span> {{ sort.area }}
-              </li>
+          <div class="col-6">
+            <ul class="debris-sort">
+              <li></li>
             </ul>
-            <div ref="mapContainer" class="map-container"></div>
           </div>
-          <div class="debris-data col-12 col-lg-6">
-            <div class="clean-tons">
-              <span class="debris-word">已清理</span>
-              <span class="debris-num">9,090</span>
-              <span class="debris-word">噸海廢</span>
-            </div>
-            <div class="clean-attend">
-              <span class="debris-word">參與人數</span>
-              <span class="debris-num">20,751</span>
-              <span class="debris-word">人次</span>
-            </div>
-            <div class="clean-session">
-              <span class="debris-word">總共</span>
-              <span class="debris-num">5,121</span>
-              <span class="debris-word">場次</span>
-            </div>
-            <p>*皆為本年度資訊，與海洋委員會海洋保育署資料同步</p>
+          <div class="col-6">
           </div>
-        </div>
-      </div>
-    </section>
-    <section class="section section-comparation">
-      <div class="container">
-        <h3>
-            COMPARATION<br>
-            對比照
-        </h3>
-      </div>
-      <div class="control"></div>
-      <div class="dirty">
-        <img src="../../public/img/index/compare-dirty.png" alt="垃圾海洋">
-      </div>
-      <div class="clean">
-        <img src="../../public/img/index/compare-clean.png" alt="乾淨海洋">
-      </div>
-    </section>
-    <section class="section section-survey">
-      <div class="container">
-        <div class="survey-card">
-          <h3>海廢知多少</h3>
-          <p>透過回答以下5個問題,讓我們一起檢視您對海洋環保的了解程度。這個測驗旨在幫助您:</p>
-            <ul>
-              <li>若得分低於80分,建議您先前往我們的教育中心,深入學習海洋環保知識,充實自己的環保知識基礎,再參與實際的環保行動,讓您的每一份努力都更有意義。</li>
-              <li>若得分高於或等於80分,歡迎您前往我們的活動頁面,尋找適合您的海洋保護行動,用實際行動為海洋盡一份心力。</li>
-            </ul>
-          <p>
-            無論結果如何,我們都感謝您對海洋環保的關注和努力。了解自己的知識水平,是成為一個負責任的環保行動者的第一步。讓我們攜手,以知識和行動,共創潔淨、永續的海洋環境!<br>
-            準備好開始這趟自我探索之旅了嗎?現在就開始測驗吧! 
-          </p>
-          <button>立即測驗</button>
         </div>
       </div>
     </section>
   </main>
 </template>
-
-
-<script>
-import * as d3 from 'd3';
-import * as topojson from 'topojson-client';
-
-export default {
-  data() {
-    return {
-      hebrisSort: [
-        { id: 1, area: '全台灣' },
-        { id: 2, area: '北部' },
-        { id: 3, area: '中部' },
-        { id: 4, area: '南部' },
-        { id: 5, area: '東部' },
-        { id: 6, area: '離島' }
-      ],
-    };
-  },
-  mounted() {
-    this.initMap();
-    window.addEventListener('resize', this.resizeMap);
-  },
-  beforeUnmount() {
-    window.removeEventListener('resize', this.resizeMap);
-  },
-  methods: {
-    async initMap() {
-      // 獲取地圖容器的引用
-      const mapContainer = this.$refs.mapContainer;
-
-      // 設置地圖的尺寸
-      const width = mapContainer.clientWidth;
-      const height = mapContainer.clientHeight;
-
-      // 創建 SVG 元素
-      const svg = d3.select(mapContainer)
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-      // 定義投影
-      const projection = d3.geoMercator()
-        .center([120, 23.5])
-        .scale(6000)
-        .translate([width / 2, height / 2]);
-
-      // 定義路徑生成器
-      const path = d3.geoPath().projection(projection);
-
-      // 讀取 TopoJSON 文件
-      const topoData = await d3.json('../../public/localjson/map/twCounty2010.topo.json');
-
-      // 轉換 TopoJSON 為 GeoJSON
-      const geoData = topojson.feature(topoData, topoData.objects.layer1);
-
-      // 繪製地圖
-      svg.selectAll('path')
-        .data(geoData.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .attr('fill', '#005FA1')
-        .attr('stroke', 'white');
-    },
-    resizeMap() {
-      // 移除現有的 SVG 元素
-      const mapContainer = this.$refs.mapContainer;
-      d3.select(mapContainer).select('svg').remove();
-
-      // 重新初始化地圖
-      this.initMap();
-    }
-  }
-};
-</script>
